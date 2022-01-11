8f99faad23f70a1821b80fbfd41b80fe6408d682
InnocentWahome <innocentwahome@gmail.com>
2022-01-12 00:00:33 +0300

    test datagrid mui

--- client/src/pages/admin/entries/article.jsx
+++ client/src/pages/admin/entries/article.jsx
@@ -1,14 +1,41 @@
-import * as React from "react"
+// const AddQualificationsPage = () => {
+//   return (
+//     <AdminLayout>
+//       <div className="container pt-6 pl-6">
+//           <p>Here's all the articles we have</p>
+//       </div>
+//     </AdminLayout>
+//   )
+// }
+// export default AddQualificationsPage
 
+import * as React from "react"
+import { DataGrid } from "@mui/x-data-grid"
+import { useDemoData } from "@mui/x-data-grid-generator"
 import AdminLayout from "../../../layouts/AdminLayout"
 
-const AddQualificationsPage = () => {
+export default function PageSizeCustomOptions() {
+  const [pageSize, setPageSize] = React.useState(5)
+
+  const { data } = useDemoData({
+    dataSet: "Commodity",
+    rowLength: 100,
+    maxColumns: 6,
+  })
+
   return (
     <AdminLayout>
-      <div className="container pt-6 pl-6">
-          <p>Here's all the articles we have</p>
+      <div class="container">
+        <div style={{ height: 650, width: "60%" }}>
+          <DataGrid
+            pageSize={pageSize}
+            onPageSizeChange={newPageSize => setPageSize(newPageSize)}
+            rowsPerPageOptions={[5, 10, 20]}
+            pagination
+            {...data}
+          />
+        </div>
       </div>
     </AdminLayout>
   )
 }
-export default AddQualificationsPage
