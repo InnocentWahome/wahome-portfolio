47fb4ba047fcfe8e607aabe4dd0dc6abfca181cd
InnocentWahome <innocentwahome@gmail.com>
2022-01-30 23:56:39 +0300

    fail: graphql problem

--- client/src/pages/test.jsx
+++ client/src/pages/test.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
 import PageLayout from "../layouts/PageLayout"
 
 const IndexPage = ({ data }) => (
@@ -7,27 +6,8 @@ const IndexPage = ({ data }) => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <ul>
-      {data.allStrapiArticle.edges.map(document => (
-        <li key={document.node.id}>
-          <p>{document.node.id}</p>
-        </li>
-      ))}
-    </ul>
+    
   </PageLayout>
 )
 
 export default IndexPage
-
-export const pageQuery = graphql`
-  query IndexQuery {
-    allStrapiArticle {
-      edges {
-        node {
-          id
-          id
-        }
-      }
-    }
-  }
-`
